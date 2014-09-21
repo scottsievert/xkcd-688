628bfd239ee319f78a76ded6de8cda06ad386409
Remi Rampin <remirampin@gmail.com>
2014-09-20 20:22:31 -0400

    PEP8

--- meta.py
+++ meta.py
@@ -7,6 +7,7 @@ from pylab import axis, figure, imshow, ion, set_cmap, title
 from scipy.misc import imread, imresize
 from time import sleep
 
+
 def sector_mask(shape=None, center=(200, 300), percentage=0.25, radius=100):
     """
     Return a boolean mask for a circular sector. The start/stop angles in  
@@ -15,37 +16,44 @@ def sector_mask(shape=None, center=(200, 300), percentage=0.25, radius=100):
     dc = 120
     angle_range = (dc, dc + percentage * 360)
 
-    x,y = np.ogrid[:shape[0],:shape[1]]
-    cx,cy = center
-    tmin,tmax = np.deg2rad(angle_range)
+    x, y = np.ogrid[:shape[0], :shape[1]]
+    cx, cy = center
+    tmin, tmax = np.deg2rad(angle_range)
 
     # ensure stop angle > start angle
     if tmax < tmin:
-            tmax += 2*np.pi
+            tmax += 2 * np.pi
 
     # convert cartesian --> polar coordinates
-    r2 = (x-cx)*(x-cx) + (y-cy)*(y-cy)
-    theta = np.arctan2(x-cx,y-cy) - tmin
+    r2 = (x - cx) * (x - cx) + (y - cy) * (y - cy)
+    theta = np.arctan2(x - cx, y - cy) - tmin
 
     # wrap angles between 0 and 2*pi
-    theta %= (2*np.pi)
+    theta %= (2 * np.pi)
 
     # circular mask
-    circmask = r2 <= radius*radius
+    circmask = r2 <= radius * radius
 
     # angular mask
-    anglemask = theta <= (tmax-tmin)
+    anglemask = theta <= (tmax - tmin)
 
     return circmask * anglemask
+
+
 def draw_circle(percentage, image, r=70, center=(90, 148)):
-    mask = sector_mask(shape=image.shape, percentage=percentage, radius=r, center=center)
+    mask = sector_mask(shape=image.shape, percentage=percentage, radius=r,
+                       center=center)
     image[mask] = 1
     return image
+
+
 def draw_bars(panel_black, comic, height=400):
     base = 154
     for i in arange(3):
         bar_height = round(height * panel_black[i])
-        comic[base-bar_height:base, 300+60*i:300+60*i+30] = 1
+        comic[base - bar_height:base, 300 + 60 * i:300 + 60 * i + 30] = 1
+
+
 def draw_figure():
     # updating first panel
     number_black = argwhere(comic > BLACK).shape[0]
@@ -55,7 +63,8 @@ def draw_figure():
     # updating second panel
     panel_black = zeros(3)
     for i in arange(3):
-        panel_black[i] = argwhere(comic[:, i*WIDTH:(i+1)*WIDTH] > BLACK).shape[0]
+        indices = argwhere(comic[:, i * WIDTH:(i + 1) * WIDTH] > BLACK)
+        panel_black[i] = indices.shape[0]
         panel_black[i] *= 1 / (WIDTH * HEIGHT)
     draw_bars(panel_black, comic)
 
@@ -66,7 +75,8 @@ def draw_figure():
     resized[:, -1] = 1
     rn, rm = resized.shape
     base_x, base_y = (80, 530)
-    comic[base_x:base_x+rn, base_y:base_y+rm] = resized
+    comic[base_x:base_x + rn, base_y:base_y + rm] = resized
+
 
 if __name__ == "__main__":
     panels = 3
@@ -82,17 +92,15 @@ if __name__ == "__main__":
     # for drawnow
     S = 12
     ion()
-    figure(figsize=(S, S/3))
+    figure(figsize=(S, S / 3))
+
     def update_image():
         set_cmap('binary')
-        imshow(comic)#, interpolation='nearest')
+        imshow(comic)
         axis('off')
-        title('Iteration %d' % (i+1))
-        
+        title('Iteration %d' % (i + 1))
 
     for i in arange(7):
         draw_figure()
         drawnow(update_image)
         sleep(1)
-
-
